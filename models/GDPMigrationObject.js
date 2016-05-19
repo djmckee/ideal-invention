281b04770596bb27591f1ea315974950544445b9
Dylan McKee <dylan@djmckee.co.uk>
2016-05-19 20:13:33 +0100

    Basic data table and graph comparison works

---
+++ models/GDPMigrationObject.js
@@ -0,0 +1,11 @@
+// Constructor
+function GDPMigrationObject(year, refugees, migration, growth) {
+  this.year = year;
+  this.refugees = refugees;
+  this.totalMigration = migration;
+  this.growth = growth;
+
+}
+
+// export the class
+module.exports = GDPMigrationObject;
