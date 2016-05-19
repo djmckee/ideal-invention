70fe245d91ce2f3692330c52deb3fa2a87e83514
Dylan McKee <dylan@djmckee.co.uk>
2016-05-19 20:13:10 +0100

    Basic data table and graph comparison works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,110 @@
 // Client-side JavaScript goes here!!!
 
+
+var myLineChart;
+
 /**
 * Perform a quick data refresh from the API via AJAX. Then display the data in
 * the table, and in a chart.js graph.
 */
 function refreshData() {
+    console.log('refreshData()');
+    // Remove existing migration entries...
+    $('.migration-table-entry').remove();
+    var fromCountryId = $('#fromCountryDropdown').val();
+    var toCountryId = $('#toCountryDropdown').val();
 
+    var queryString = "get_data?fromCountryId=" + fromCountryId + "&toCountryId=" + toCountryId;
+    console.log(queryString);
+
+    $.get(queryString, function(data) {
+        console.log(data);
+        // Add table entries...
+        var yearLabels = [];
+        var gdpValues = [];
+        var refugeeCounts = [];
+
+        data.forEach(function(item){
+            yearLabels.push(String(item.year));
+            gdpValues.push(item.growth * 100);
+            refugeeCounts.push(item.refugees);
+        });
+
+        var reversedData = data.reverse();
+
+        reversedData.forEach(function(item){
+            var plusIcon = '';
+            if (item.growth > 0) {
+                plusIcon = '+';
+            }
+            var element = '<tr class="migration-table-entry"><td>' + item.year + '</td><td>' + item.refugees + '</td><td>' + item.totalMigration + '</td><td>' + plusIcon + item.growth.toFixed(2) + '</td></tr>';
+            $('#migration-table-body').append(element);
+        });
+
+        // TODO: Graph!
+        var ctx = $("#chart");
+
+
+        var data = {
+            labels: yearLabels,
+            datasets: [
+                {
+                    label: "∆GDP",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: "rgba(192, 57, 43,1.0)",
+                    borderColor: "rgba(192, 57, 43,1.0)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(192, 57, 43,1.0)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(192, 57, 43,1.0)",
+                    pointHoverBorderColor: "rgba(192, 57, 43,1.0)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: gdpValues,
+                },
+                {
+                    label: "Refugees",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: refugeeCounts,
+                }
+            ]
+        };
+
+        if (myLineChart != null) {
+            myLineChart.destroy();
+            myLineChart = null;
+        }
+
+        myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: null
+        });
+
+    });
 }
 
 /**
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var Year = require('../models/Year.js');
 var Country = require('../models/Country.js');
 var Migration = require('../models/Migration.js');
 var GDPValue = require('../models/GDPValue.js');
-
+var GDPMigrationObject = require('../models/GDPMigrationObject.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,7 +25,64 @@ router.get('/', function(req, res, next) {
 router.get('/get_data', function(req, res, next) {
     // TODO: Implement some clever stuff.
 
-    res.json({});
+    var fromCountryId = req.query.fromCountryId;
+    var toCountryId = req.query.toCountryId;
+    Country.findById(fromCountryId, function (err, fromCountry) {
+        Country.findById(toCountryId, function (err, toCountry) {
+            // Fetch GDPs...
+            GDPValue.find({country: toCountry.name}, function(err, gdpValues) {
+                if (!err){
+                    // Fetch migration data...
+
+                    Migration.find({toCountry: toCountry.name, fromCountry: fromCountry.name}, function(err, migrationValues) {
+                        if (!err){
+                            // Stitch together migration and gdp values...
+                            var array = [];
+
+                            gdpValues.forEach(function(value){
+                                var migrationData = findMigrationForYear(migrationValues, value.year);
+
+                                var refugeeFigure = 0;
+                                var migrantTotalFigure = 0;
+
+                                if (migrationData != null) {
+                                    refugeeFigure = migrationData.refugees;
+                                    migrantTotalFigure = migrationData.total;
+                                }
+
+                                var object = new GDPMigrationObject(value.year, refugeeFigure, migrantTotalFigure, value.growth);
+
+                                array.push(object);
+
+                            });
+
+                            res.json(array);
+
+                        } else {
+                            throw err;
+                        }
+                    });
+
+                } else {
+                    throw err;
+                }
+            });
+        });
+    });
+
 });
+
+
+function findMigrationForYear(migrations, year) {
+    var migration = null;
+
+    migrations.forEach(function(value){
+        if (value.year == year) {
+            migration = value;
+        }
+    });
+
+    return migration;
+}
 
 module.exports = router;
